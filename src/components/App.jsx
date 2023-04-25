848f8049120bdbac38a73ba5a2256a1aa1cb7f9c
Edijs <edijsapse@gmail.com>
2023-04-25 10:02:33 +0300

    Scroll to top on page change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import Sidebar from "./Sidebar";
 import Forecast from "../pages/Forecast";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import WeatherContext, {
   CITIES_PAGE,
   FORECAST_PAGE,
@@ -9,6 +9,11 @@ import Cities from "../pages/Cities";
 
 const App = () => {
   const context = useContext(WeatherContext);
+
+  useEffect(() => {
+    window.scroll({ behavior: "smooth", top: 0 });
+  }, [context.selectedPage]);
+
   return (
     <div className="w-full mx-6 flex py-0 pt-6 pb-24 lg:py-16 lg:space-x-8 lg:container lg:mx-auto">
       <Sidebar />
